bf3d53a3669317efa940e038ee50c6d19bc8babb
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-08 17:27:40 +0200

    Create User entity class

---
+++ src/main/java/com/knook/model/User.java
@@ -0,0 +1,62 @@
+package com.knook.model;
+
+import com.google.gson.annotations.Expose;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @Expose
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id", nullable=false)
+    private Long id;
+    
+    @Expose
+    @Column(name="username", nullable=false)
+    private String email;
+
+    @Column(name="password", nullable=false)
+    private String password;
+    
+    public User() {
+        this.id = 0l;
+        this.email = "";
+        this.password = "";
+    }
+
+    public User(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public final void setPassword(String password) {
+        this.password = password;
+    }
+}
