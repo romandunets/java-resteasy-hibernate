9da2009565559d3bd80b2a706b8a726afe92b8aa
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-09 20:14:21 +0200

    Add SQL query for seeding knook database

---
+++ src/main/resources/sql/seed_database.sql
@@ -0,0 +1,3 @@
+INSERT INTO `users` (`email`, `password`) VALUES ("testuser1@example.com", "password");
+INSERT INTO `users` (`email`, `password`) VALUES ("testuser2@example.com", "password");
+INSERT INTO `users` (`email`, `password`) VALUES ("testuser3@example.com", "password");
