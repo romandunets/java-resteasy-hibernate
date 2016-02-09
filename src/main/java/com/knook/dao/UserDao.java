fff68e62b258f4237012054eeb734fecdf73fe07
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-09 20:21:08 +0200

    Add list method to UserDao class

--- src/main/java/com/knook/dao/UserDao.java
+++ src/main/java/com/knook/dao/UserDao.java
@@ -11,6 +11,31 @@ public class UserDao {
     
     protected SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
     
+    public List<User> list() {
+        List<User> users = null;
+        Session session = null;
+
+        try {
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            Query query = session.createQuery("from User u");
+            users = query.list();
+            session.getTransaction().commit();
+        }
+        catch (Exception exception) {
+           if (session != null) {
+               session.getTransaction().rollback();
+           }
+        }
+        finally {
+            if (session != null) {
+               session.close();
+            }
+        }
+
+        return users;
+    }
+    
     
     
 }
