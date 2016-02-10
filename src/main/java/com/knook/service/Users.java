20ff22270c983323f56779a6f113c4d6f4c8cd8e
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-10 17:38:53 +0200

    Add create methodUser for Users service class

--- src/main/java/com/knook/service/Users.java
+++ src/main/java/com/knook/service/Users.java
@@ -3,11 +3,15 @@ package com.knook.service;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.knook.dao.UserDao;
+import com.knook.model.User;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("users")
 public class Users {
@@ -28,5 +32,18 @@ public class Users {
     @Produces(MediaType.APPLICATION_JSON)
     public String getUser(@PathParam("id") Long id) {
         return gson.toJson(userDao.get(id));
+    }
+
+    @POST
+    @Path("/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createUser(User user) {
+        if (userDao.create(user)) {
+            return Response.status(Response.Status.OK).build();
+        }
+        else {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
     }
 }
