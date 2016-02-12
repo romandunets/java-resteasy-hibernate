82b9bd525cda5685a2e2e0589806cfef543fd36a
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-12 17:38:54 +0200

    Add SQL query to seed notes

--- src/main/resources/sql/seed_database.sql
+++ src/main/resources/sql/seed_database.sql
@@ -1,3 +1,7 @@
 INSERT INTO `users` (`email`, `password`) VALUES ("testuser1@example.com", "password");
 INSERT INTO `users` (`email`, `password`) VALUES ("testuser2@example.com", "password");
 INSERT INTO `users` (`email`, `password`) VALUES ("testuser3@example.com", "password");
+
+INSERT INTO `notes` (`title`, `content`) VALUES ("Java", "write once, run anywhere");
+INSERT INTO `notes` (`title`, `content`) VALUES ("JavaScript", "client-side programming language");
+INSERT INTO `notes` (`title`, `content`) VALUES ("Ruby", "dynamic, open source programming language");
