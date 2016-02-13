c0e02ab0c6cd80fa4c2518b842a6060254540a53
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-13 20:45:18 +0200

    Add list method to NoteDao class

--- src/main/java/com/knook/dao/NoteDao.java
+++ src/main/java/com/knook/dao/NoteDao.java
@@ -11,6 +11,29 @@ public class NoteDao {
     
     protected SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
     
-    
+    public List<Note> list() {
+        List<Note> notes = null;
+        Session session = null;
+
+        try {
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            Query query = session.createQuery("from Note n");
+            notes = query.list();
+            session.getTransaction().commit();
+        }
+        catch (Exception exception) {
+           if (session != null) {
+               session.getTransaction().rollback();
+           }
+        }
+        finally {
+            if (session != null) {
+               session.close();
+            }
+        }
+
+        return notes;
+    }
     
 }
