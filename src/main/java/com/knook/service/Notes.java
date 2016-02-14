15801d8c4929a26ccb052da265d4961b4eadde34
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-14 21:54:29 +0200

    Add list method for Notes service class

--- src/main/java/com/knook/service/Notes.java
+++ src/main/java/com/knook/service/Notes.java
@@ -3,14 +3,24 @@ package com.knook.service;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.knook.dao.NoteDao;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("notes")
 public class Notes {
-    
+
     private GsonBuilder builder = new GsonBuilder();
     private Gson gson = builder.create();
-    
+
     private NoteDao noteDao = new NoteDao();
-    
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String listNotes() {
+        return gson.toJson(noteDao.list());
+    }
+
 }
