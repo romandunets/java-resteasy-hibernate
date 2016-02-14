900554bfcf9e49733e5c674574d3f73045974da1
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-14 21:57:56 +0200

    Add get method for Notes service class

--- src/main/java/com/knook/service/Notes.java
+++ src/main/java/com/knook/service/Notes.java
@@ -5,6 +5,7 @@ import com.google.gson.GsonBuilder;
 import com.knook.dao.NoteDao;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -21,6 +22,13 @@ public class Notes {
     @Produces(MediaType.APPLICATION_JSON)
     public String listNotes() {
         return gson.toJson(noteDao.list());
+    }
+    
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getNotes(@PathParam("id") Long id) {
+        return gson.toJson(noteDao.get(id));
     }
 
 }
