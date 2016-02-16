8c674d152300093d0ee53697e626f5acc4e7d311
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-16 22:27:49 +0200

    Add user id column to notes table

--- src/main/resources/sql/create_database.sql
+++ src/main/resources/sql/create_database.sql
@@ -12,5 +12,7 @@ CREATE TABLE IF NOT EXISTS `notes` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(45) NOT NULL,
   `content` TEXT NOT NULL,
-  PRIMARY KEY (`id`)
+  `user_id` BIGINT(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_users_to_notes` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
