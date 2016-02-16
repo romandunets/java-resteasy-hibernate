29b90eb8353cb810113f9380c1fa1d53a20621a3
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-16 22:28:13 +0200

    Add seeds for user id column in notes table

--- src/main/resources/sql/seed_database.sql
+++ src/main/resources/sql/seed_database.sql
@@ -2,6 +2,6 @@ INSERT INTO `users` (`email`, `password`) VALUES ("testuser1@example.com", "pass
 INSERT INTO `users` (`email`, `password`) VALUES ("testuser2@example.com", "password");
 INSERT INTO `users` (`email`, `password`) VALUES ("testuser3@example.com", "password");
 
-INSERT INTO `notes` (`title`, `content`) VALUES ("Java", "write once, run anywhere");
-INSERT INTO `notes` (`title`, `content`) VALUES ("JavaScript", "client-side programming language");
-INSERT INTO `notes` (`title`, `content`) VALUES ("Ruby", "dynamic, open source programming language");
+INSERT INTO `notes` (`title`, `content`, `user_id`) VALUES ("Java", "write once, run anywhere", 1);
+INSERT INTO `notes` (`title`, `content`, `user_id`) VALUES ("JavaScript", "client-side programming language", 1);
+INSERT INTO `notes` (`title`, `content`, `user_id`) VALUES ("Ruby", "dynamic, open source programming language", 2);
