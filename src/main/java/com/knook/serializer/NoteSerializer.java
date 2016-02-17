a117e5f47367e9fc59707253c2522d721abff0a9
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-17 18:43:19 +0200

    Add custom GSON serializer for note entity

---
+++ src/main/java/com/knook/serializer/NoteSerializer.java
@@ -0,0 +1,24 @@
+package com.knook.serializer;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.knook.model.Note;
+import java.lang.reflect.Type;
+
+public class NoteSerializer implements JsonSerializer<Note> {
+
+    @Override
+    public JsonElement serialize(Note src, Type typeOfSrc, JsonSerializationContext context) {
+        JsonObject object = new JsonObject();
+
+        object.addProperty("id", src.getId());
+        object.addProperty("title", src.getTitle());
+        object.addProperty("content", src.getContent());
+        object.addProperty("user_id", src.getUser().getId());
+
+        return object;
+    }
+
+}
