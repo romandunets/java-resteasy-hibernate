13baeda39c4b4171952617eb0a92c45c005044ae
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-17 18:43:05 +0200

    Add custom GSON serializer for user entity

---
+++ src/main/java/com/knook/serializer/UserSerializer.java
@@ -0,0 +1,22 @@
+package com.knook.serializer;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.knook.model.User;
+import java.lang.reflect.Type;
+
+public class UserSerializer implements JsonSerializer<User> {
+
+    @Override
+    public JsonElement serialize(User src, Type typeOfSrc, JsonSerializationContext context) {
+        JsonObject object = new JsonObject();
+
+        object.addProperty("id", src.getId());
+        object.addProperty("email", src.getEmail());
+
+        return object;
+    }
+
+}
