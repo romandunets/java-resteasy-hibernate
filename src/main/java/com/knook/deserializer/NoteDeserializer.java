1ae57966a43a72495be5ee8ab4cb5a89cf13afa5
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-18 17:13:56 +0200

    Add custom GSON deserializer for note entity

---
+++ src/main/java/com/knook/deserializer/NoteDeserializer.java
@@ -0,0 +1,24 @@
+package com.knook.deserializer;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.knook.dao.UserDao;
+import com.knook.model.Note;
+import com.knook.model.User;
+import java.lang.reflect.Type;
+
+public class NoteDeserializer implements JsonDeserializer<Note> {
+
+    @Override
+    public Note deserialize(JsonElement json, Type type, JsonDeserializationContext context) throws JsonParseException {
+        String title = json.getAsJsonObject().get("title").getAsString();
+        String content = json.getAsJsonObject().get("content").getAsString();
+        
+        Note note = new Note(title, content);
+
+        return note;
+    }
+
+}
