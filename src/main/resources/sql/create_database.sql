97f68c1acee867d869434accd3020a27727f26f0
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-27 22:34:02 +0200

    Add SQL quesry for creating groupes table

--- src/main/resources/sql/create_database.sql
+++ src/main/resources/sql/create_database.sql
@@ -20,3 +20,14 @@ CREATE TABLE IF NOT EXISTS `notes` (
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_users_to_notes` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `groupes` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `description` TEXT NOT NULL,
+  `user_id` BIGINT(20) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_users_to_groupes` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+);
