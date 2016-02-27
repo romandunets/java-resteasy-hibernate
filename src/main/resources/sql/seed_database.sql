7967e1d4ecb444fa53690cc3dbfdcc898cc147ea
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-27 22:34:20 +0200

    Add SQL quesry for seeding groupes table

--- src/main/resources/sql/seed_database.sql
+++ src/main/resources/sql/seed_database.sql
@@ -5,3 +5,7 @@ INSERT INTO `users` (`email`, `password`) VALUES ("testuser3@example.com", "pass
 INSERT INTO `notes` (`title`, `content`, `user_id`) VALUES ("Java", "write once, run anywhere", 1);
 INSERT INTO `notes` (`title`, `content`, `user_id`) VALUES ("JavaScript", "client-side programming language", 1);
 INSERT INTO `notes` (`title`, `content`, `user_id`) VALUES ("Ruby", "dynamic, open source programming language", 2);
+
+INSERT INTO `groupes` (`name`, `description`, `user_id`) VALUES ("Java", "Learning Java", 1);
+INSERT INTO `groupes` (`name`, `description`, `user_id`) VALUES ("JavaScript", "Learning JavaScript", 1);
+INSERT INTO `groupes` (`name`, `description`, `user_id`) VALUES ("Ruby", "Learning Ruby", 2);
