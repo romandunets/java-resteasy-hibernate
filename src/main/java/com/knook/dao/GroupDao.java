accb580c72776b29c1d0e5d5c343447964e5c517
Roman Dunets <roman.dunets.email@gmail.com>
2016-03-03 17:17:35 +0200

    Add Group dao class

---
+++ src/main/java/com/knook/dao/GroupDao.java
@@ -0,0 +1,14 @@
+package com.knook.dao;
+
+import com.knook.model.Group;
+
+public class GroupDao extends AbstractDao<Group> {
+
+    public GroupDao() {
+        super(Group.class);
+    }
+    
+    @Override
+    protected void initializeEntity(Group group) {}
+
+}
