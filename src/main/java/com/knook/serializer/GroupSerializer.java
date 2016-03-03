746d5ed6007144848d8901b83abe3c892b8e9c6c
Roman Dunets <roman.dunets.email@gmail.com>
2016-03-03 17:18:16 +0200

    Add GroupSerializer class

---
+++ src/main/java/com/knook/serializer/GroupSerializer.java
@@ -0,0 +1,26 @@
+package com.knook.serializer;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.knook.model.Group;
+import java.lang.reflect.Type;
+
+public class GroupSerializer implements JsonSerializer<Group> {
+
+    @Override
+    public JsonElement serialize(Group src, Type typeOfSrc, JsonSerializationContext context) {
+        JsonObject object = new JsonObject();
+
+        object.addProperty("id", src.getId());
+        object.addProperty("name", src.getName());
+        object.addProperty("description", src.getDescription());
+        object.addProperty("user_id", src.getUser().getId());
+        object.addProperty("created_at", src.getCreatedAt().toString());
+        object.addProperty("updated_at", src.getUpdatedAt().toString());
+
+        return object;
+    }
+
+}
