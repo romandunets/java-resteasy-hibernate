0b22b171a776f173c11ec1bf5c6208c1fee329c9
Roman Dunets <roman.dunets.email@gmail.com>
2016-03-04 21:34:31 +0200

    Add list method for Groupes service class

--- src/main/java/com/knook/service/Groups.java
+++ src/main/java/com/knook/service/Groups.java
@@ -5,7 +5,11 @@ import com.google.gson.GsonBuilder;
 import com.knook.dao.GroupDao;
 import com.knook.model.Group;
 import com.knook.serializer.GroupSerializer;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("users/{user_id}/groups")
 public class Groups {
@@ -16,5 +20,12 @@ public class Groups {
     private Gson gson = builder.create();
 
     private GroupDao groupDao = new GroupDao();
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String list(@PathParam("user_id") Long userId) {
+        return gson.toJson(groupDao.listForUser(userId));
+    }
 
 }
