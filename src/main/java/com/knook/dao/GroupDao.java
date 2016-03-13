3cc10b37d1f023dfc3bac1a837c74be11c9946bb
Roman Dunets <roman.dunets.email@gmail.com>
2016-03-13 23:03:39 +0200

    Remove extra listForUser methods in NoteDao and GroupDao classes

--- src/main/java/com/knook/dao/GroupDao.java
+++ src/main/java/com/knook/dao/GroupDao.java
@@ -2,7 +2,6 @@ package com.knook.dao;
 
 import com.knook.model.Group;
 import com.knook.model.User;
-import java.util.List;
 import org.hibernate.Hibernate;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -11,32 +10,6 @@ public class GroupDao extends AbstractDao<Group> {
 
     public GroupDao() {
         super(Group.class);
-    }
-
-    public List<Group> listForUser(Long userId) {
-        List<Group> groupes = null;
-        Session session = null;
-
-        try {
-            session = sessionFactory.openSession();
-            session.beginTransaction();
-            Query query = session.createQuery("from Group g where g.user.id = :USER_ID");
-            query.setParameter("USER_ID", userId);
-            groupes = query.list();
-            session.getTransaction().commit();
-        }
-        catch (Exception exception) {
-           if (session != null) {
-               session.getTransaction().rollback();
-           }
-        }
-        finally {
-            if (session != null) {
-               session.close();
-            }
-        }
-
-        return groupes;
     }
 
     public Group getForUser(Long userId, Long id) {
--- src/main/java/com/knook/dao/NoteDao.java
+++ src/main/java/com/knook/dao/NoteDao.java
@@ -2,7 +2,6 @@ package com.knook.dao;
 
 import com.knook.model.Note;
 import com.knook.model.User;
-import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
@@ -10,32 +9,6 @@ public class NoteDao extends AbstractDao<Note> {
 
     public NoteDao() {
         super(Note.class);
-    }
-
-    public List<Note> listForUser(Long userId) {
-        List<Note> notes = null;
-        Session session = null;
-
-        try {
-            session = sessionFactory.openSession();
-            session.beginTransaction();
-            Query query = session.createQuery("from Note n where n.user.id = :USER_ID");
-            query.setParameter("USER_ID", userId);
-            notes = query.list();
-            session.getTransaction().commit();
-        }
-        catch (Exception exception) {
-           if (session != null) {
-               session.getTransaction().rollback();
-           }
-        }
-        finally {
-            if (session != null) {
-               session.close();
-            }
-        }
-
-        return notes;
     }
 
     public Note getForUser(Long userId, Long id) {
