a207d4487a163da6a41af9419f614d3eb0eb5b1c
Roman Dunets <roman.dunets.email@gmail.com>
2016-03-14 23:26:32 +0200

    remove createForUser method from GroupDao class

--- src/main/java/com/knook/dao/GroupDao.java
+++ src/main/java/com/knook/dao/GroupDao.java
@@ -39,33 +39,6 @@ public class GroupDao extends AbstractDao<Group> {
         return group;
     }
 
-    public boolean createForUser(Group group, Long userId) {
-        Boolean success = false;
-        Session session = null;
-        User user = new User(userId);
-        group.setUser(user);
-
-        try {
-            session = sessionFactory.openSession();
-            session.beginTransaction();
-            session.persist(group);
-            session.getTransaction().commit();
-            success = true;
-        }
-        catch (Exception exception) {
-           if (session != null) {
-               session.getTransaction().rollback();
-           }
-        }
-        finally {
-            if (session != null) {
-               session.close();
-            }
-        }
-
-        return success;
-    }
-
     public boolean updateForUser(Group group, Long userId) {
         Boolean success = false;
         Session session = null;
