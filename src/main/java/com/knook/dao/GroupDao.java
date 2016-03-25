b6c26e48cd0f59c6bf2e8b1cfe2064e527c1b6f1
Roman Dunets <roman.dunets.email@gmail.com>
2016-03-25 23:26:24 +0200

    Remove extra methods from GroupDao class

--- src/main/java/com/knook/dao/GroupDao.java
+++ src/main/java/com/knook/dao/GroupDao.java
@@ -1,69 +1,11 @@
 package com.knook.dao;
 
 import com.knook.model.Group;
-import com.knook.model.User;
-import org.hibernate.Hibernate;
-import org.hibernate.Query;
-import org.hibernate.Session;
 
 public class GroupDao extends AbstractDao<Group> {
 
     public GroupDao() {
         super(Group.class);
-    }
-
-    public Group getForUser(Long userId, Long id) {
-        Group group = null;
-        Session session = null;
-
-        try {
-            session = sessionFactory.openSession();
-            session.beginTransaction();
-            Query query = session.createQuery("from Group g where g.user.id = :USER_ID and g.id = :ID");
-            query.setParameter("USER_ID", userId);
-            query.setParameter("ID", id);
-            group = (Group) query.uniqueResult();
-            session.getTransaction().commit();
-        }
-        catch (Exception exception) {
-           if (session != null) {
-               session.getTransaction().rollback();
-           }
-        }
-        finally {
-            if (session != null) {
-               session.close();
-            }
-        }
-
-        return group;
-    }
-
-    public boolean updateForUser(Group group, Long userId) {
-        Boolean success = false;
-        Session session = null;
-        User user = new User(userId);
-        group.setUser(user);
-
-        try {
-            session = sessionFactory.openSession();
-            session.beginTransaction();
-            session.merge(group);
-            session.getTransaction().commit();
-            success = true;
-        }
-        catch (Exception exception) {
-           if (session != null) {
-               session.getTransaction().rollback();
-           }
-        }
-        finally {
-            if (session != null) {
-               session.close();
-            }
-        }
-
-        return success;
     }
 
     @Override
