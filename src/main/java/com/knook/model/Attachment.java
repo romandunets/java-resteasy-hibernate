d2cb082d0eefea766fd819780a7dbfd219a9b7e2
Roman Dunets <roman.dunets.email@gmail.com>
2016-04-17 21:51:41 +0300

    remove extra imports from Attachment model class

--- src/main/java/com/knook/model/Attachment.java
+++ src/main/java/com/knook/model/Attachment.java
@@ -1,20 +1,13 @@
 package com.knook.model;
 
-import java.util.Date;
-import java.util.Set;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 @Entity
 @Table(name="attachments")
