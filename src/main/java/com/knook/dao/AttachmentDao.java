94260c69fde81c6000efb4a6ed607bb1fd19cdc7
Roman Dunets <roman.dunets.email@gmail.com>
2016-04-19 19:39:31 +0300

    Create AttachmentDao class

---
+++ src/main/java/com/knook/dao/AttachmentDao.java
@@ -0,0 +1,11 @@
+package com.knook.dao;
+
+import com.knook.model.Attachment;
+
+public class AttachmentDao extends AbstractDao<Attachment> {
+
+    public AttachmentDao() {
+        super(Attachment.class);
+    }
+
+}
