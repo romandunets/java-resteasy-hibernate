ee9c209c5aa03d1502b6bfc9f4ca6b522d4c98fb
Roman Dunets <roman.dunets.email@gmail.com>
2016-04-25 18:20:04 +0300

    Create ConnectionType model class

---
+++ src/main/java/com/knook/model/ConnectionType.java
@@ -0,0 +1,93 @@
+package com.knook.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="attachments")
+public class ConnectionType implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id", nullable=false)
+    private Long id;
+
+    @Column(name="name", nullable=false)
+    private String name;
+
+    @Column(name="description", nullable=false)
+    private String description;
+
+    @Column(name="created_at", updatable = false, nullable=false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Column(name="updated_at", nullable=false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+    public ConnectionType() {
+        this.id = 0l;
+        this.name = "";
+        this.description = "";
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
+
+    public ConnectionType(String name, String description) {
+        this.id = 0l;
+        this.name = name;
+        this.description = description;
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
