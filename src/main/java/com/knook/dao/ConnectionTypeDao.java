0bb0fddcb4a6d1a9c01af3c059754069c3c28d17
Roman Dunets <roman.dunets.email@gmail.com>
2016-04-27 18:57:52 +0300

    Create ConnectionTypeDao class

---
+++ src/main/java/com/knook/dao/ConnectionTypeDao.java
@@ -0,0 +1,11 @@
+package com.knook.dao;
+
+import com.knook.model.ConnectionType;
+
+public class ConnectionTypeDao extends AbstractDao<ConnectionType> {
+
+    public ConnectionTypeDao() {
+        super(ConnectionType.class);
+    }
+
+}
